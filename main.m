e29973a11a8c8d6acbd96c48ec3cd35e9a66735d
Allen9527466 <fff1191938336@gmail.com>
2026-09-11 03:03:00 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
-#include <substrate.h>
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
+#include <objc/runtime.h>
 
 // ===================== 代理：AVCapture SampleBuffer 替换 =====================
 @interface VirtualCamProxyDelegate : NSObject <AVCaptureVideoDataOutputSampleBufferDelegate>
@@ -17,26 +17,24 @@
     return self;
 }
 - (void)captureOutput:(AVCaptureVideoDataOutput *)output didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection{
-    // 这里替换画面，暂时原样透传，先保证插件菜单能出来
+    // 原样透传画面，后续在这里替换成你的虚拟画面
     [_originalDelegate captureOutput:output didOutputSampleBuffer:sampleBuffer fromConnection:connection];
 }
 @end
 
-CMSampleBufferRef GetVirtualSampleBuffer(void);
 void* VirtualCamProxyDelegate_alloc(id orig){
     VirtualCamProxyDelegate *p = [[VirtualCamProxyDelegate alloc] initWithOrig:orig];
     return (__bridge void*)p;
 }
 
-// ===================== 设置页面Hook占位（重点，日志会打印类名） =====================
+// ===================== 设置页面Hook占位 =====================
 static NSString * const SETTING_VC_CLASS_NAME = @"AwemeSettingsViewController";
-
-static void (*orig_tableView_didSelectRow)(id, SEL, id, NSIndexPath*);
-static NSInteger (*orig_numberOfRows)(id, SEL, id, NSInteger);
-static UITableViewCell* (*orig_cellForRow)(id, SEL, id, NSIndexPath*);
+static IMP orig_tableView_didSelectRow;
+static IMP orig_numberOfRows;
+static IMP orig_cellForRow;
 
 void hook_didSelectRow(id self, SEL _cmd, UITableView *tableView, NSIndexPath *indexPath) {
-    orig_tableView_didSelectRow(self, _cmd, tableView, indexPath);
+    ((void(*)(id,SEL,id,NSIndexPath*))orig_tableView_didSelectRow)(self, _cmd, tableView, indexPath);
     if(indexPath.section == 1 && indexPath.row == 0){
         UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"虚拟摄像头" message:@"插件设置面板" preferredStyle:UIAlertControllerStyleAlert];
         [alert addAction:[UIAlertAction actionWithTitle:@"关闭" style:UIAlertActionStyleCancel handler:nil]];
@@ -46,7 +44,7 @@ void hook_didSelectRow(id self, SEL _cmd, UITableView *tableView, NSIndexPath *i
 }
 
 NSInteger hook_numberOfRows(id self, SEL _cmd, UITableView *tableView, NSInteger section) {
-    NSInteger cnt = orig_numberOfRows(self, _cmd, tableView, section);
+    NSInteger cnt = ((NSInteger(*)(id,SEL,id,NSInteger))orig_numberOfRows)(self, _cmd, tableView, section);
     if(section == 1){
         return cnt + 1;
     }
@@ -63,22 +61,32 @@ UITableViewCell* hook_cellForRow(id self, SEL _cmd, UITableView *tableView, NSIn
     }
     if(indexPath.section ==1 && indexPath.row >0){
         NSIndexPath *oldIdx = [NSIndexPath indexPathForRow:indexPath.row -1 inSection:indexPath.section];
-        return orig_cellForRow(self, _cmd, tableView, oldIdx);
+        return ((UITableViewCell*(*)(id,SEL,id,NSIndexPath*))orig_cellForRow)(self, _cmd, tableView, oldIdx);
     }
-    return orig_cellForRow(self, _cmd, tableView, indexPath);
+    return ((UITableViewCell*(*)(id,SEL,id,NSIndexPath*))orig_cellForRow)(self, _cmd, tableView, indexPath);
 }
 
-// ===================== AVCapture Hook占位 =====================
-static void (*orig_setSampleBufferDelegate)(id, SEL, id, dispatch_queue_t);
+// ===================== AVCaptureVideoDataOutput Hook =====================
+static IMP orig_setSampleBufferDelegate;
 void hook_setSampleBufferDelegate(id self, SEL _cmd, id delegate, dispatch_queue_t queue)
 {
     NSLog(@"[VirtualCam] hook_setSampleBufferDelegate 触发");
     if(delegate){
         void* ptr = VirtualCamProxyDelegate_alloc(delegate);
         id proxy = (__bridge_transfer id)ptr;
-        orig_setSampleBufferDelegate(self, _cmd, proxy, queue);
+        ((void(*)(id,SEL,id,dispatch_queue_t))orig_setSampleBufferDelegate)(self, _cmd, proxy, queue);
     }else{
-        orig_setSampleBufferDelegate(self, _cmd, delegate, queue);
+        ((void(*)(id,SEL,id,dispatch_queue_t))orig_setSampleBufferDelegate)(self, _cmd, delegate, queue);
+    }
+}
+
+// 方法交换工具函数
+static void swizzle(Class cls, SEL originalSel, SEL newSel, IMP *outOrigIMP){
+    Method origMethod = class_getInstanceMethod(cls, originalSel);
+    Method newMethod = class_getInstanceMethod(cls, newSel);
+    if(origMethod && newMethod){
+        *outOrigIMP = method_getImplementation(origMethod);
+        method_setImplementation(origMethod, newMethod);
     }
 }
 
@@ -90,9 +98,9 @@ static void init_plugin() {
     // 尝试Hook设置页面
     Class settingVC = objc_getClass([SETTING_VC_CLASS_NAME UTF8String]);
     if(settingVC){
-        MSHookMessageEx(settingVC, @selector(tableView:numberOfRowsInSection:), (IMP)hook_numberOfRows, (IMP *)&orig_numberOfRows);
-        MSHookMessageEx(settingVC, @selector(tableView:cellForRowAtIndexPath:), (IMP)hook_cellForRow, (IMP *)&orig_cellForRow);
-        MSHookMessageEx(settingVC, @selector(tableView:didSelectRowAtIndexPath:), (IMP)hook_didSelectRow, (IMP *)&orig_tableView_didSelectRow);
+        swizzle(settingVC, @selector(tableView:numberOfRowsInSection:), @selector(hook_tableView:numberOfRowsInSection:), &orig_numberOfRows);
+        swizzle(settingVC, @selector(tableView:cellForRowAtIndexPath:), @selector(hook_tableView:cellForRowAtIndexPath:), &orig_cellForRow);
+        swizzle(settingVC, @selector(tableView:didSelectRowAtIndexPath:), @selector(hook_tableView:didSelectRowAtIndexPath:), &orig_tableView_didSelectRow);
         NSLog(@"[VirtualCam] ✅ Hook 设置页面成功！");
     }else{
         NSLog(@"[VirtualCam] ❌ 找不到类：%@，需要替换SETTING_VC_CLASS_NAME", SETTING_VC_CLASS_NAME);
@@ -101,9 +109,17 @@ static void init_plugin() {
     // Hook AVCaptureVideoDataOutput
     Class avCaptureClass = objc_getClass("AVCaptureVideoDataOutput");
     if(avCaptureClass){
-        MSHookMessageEx(avCaptureClass, @selector(setSampleBufferDelegate:queue:), (IMP)hook_setSampleBufferDelegate, &orig_setSampleBufferDelegate);
+        swizzle(avCaptureClass, @selector(setSampleBufferDelegate:queue:), @selector(hook_setSampleBufferDelegate:queue:), &orig_setSampleBufferDelegate);
         NSLog(@"[VirtualCam] ✅ Hook AVCaptureVideoDataOutput成功");
     }else{
         NSLog(@"[VirtualCam] ❌ AVCaptureVideoDataOutput class not found");
     }
 }
+
+// 给类添加hook方法定义
+@implementation NSObject (HookAdditions)
+- (NSInteger)hook_tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section { return 0; }
+- (UITableViewCell *)hook_tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath { return nil; }
+- (void)hook_tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {}
+- (void)hook_setSampleBufferDelegate:(id)delegate queue:(dispatch_queue_t)queue {}
+@end
