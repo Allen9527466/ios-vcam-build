f2456cf2bb3df2c9cece79605205337dc07aa081
Allen9527466 <fff1191938336@gmail.com>
2026-09-11 17:36:38 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,81 +1,106 @@
 #import <Foundation/Foundation.h>
-#import <CoreVideo/CoreVideo.h>
 #import <UIKit/UIKit.h>
-#include <dlfcn.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreMedia/CoreMedia.h>
+#import <CoreVideo/CoreVideo.h>
+#include <objc/runtime.h>
 
-static BOOL g_virtualCamEnable = NO;
-static OSStatus (*orig_CVPixelBufferLockBaseAddress)(CVPixelBufferRef buffer, CVPixelBufferLockFlags lockFlags);
+static BOOL g_vcamEnable = NO;
 
-OSStatus hook_CVPixelBufferLockBaseAddress(CVPixelBufferRef buffer, CVPixelBufferLockFlags lockFlags)
+static void swizzle(Class cls, SEL origSel, SEL newSel)
 {
-    OSStatus ret = orig_CVPixelBufferLockBaseAddress(buffer, lockFlags);
-    
-    if(g_virtualCamEnable && buffer)
-    {
-        size_t w = CVPixelBufferGetWidth(buffer);
-        size_t h = CVPixelBufferGetHeight(buffer);
-        if(w >= 320 && h >=320){
-            void *base = CVPixelBufferGetBaseAddress(buffer);
-            size_t stride = CVPixelBufferGetBytesPerRow(buffer);
-            uint32_t *ptr = (uint32_t*)base;
-            for(int i=0; i < w*h; i++){
-                ptr[i] = 0xFFFF0000; // BGRA蓝色
-            }
-        }
-    }
-    return ret;
+    Method origMethod = class_getInstanceMethod(cls, origSel);
+    Method newMethod = class_getInstanceMethod(cls, newSel);
+    method_exchangeImplementations(origMethod, newMethod);
 }
 
-@interface VCWindow : UIWindow
+@interface AVCaptureOutput (VCamHook)
+- (void)vcam_captureOutput:(AVCaptureOutput *)output didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection;
 @end
-@implementation VCWindow
-- (instancetype)init{
+
+@implementation AVCaptureOutput (VCamHook)
+
+- (void)vcam_captureOutput:(AVCaptureOutput *)output didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection
+{
+    // 先调用原始方法
+    [self vcam_captureOutput:output didOutputSampleBuffer:sampleBuffer fromConnection:connection];
+    
+    if (!g_vcamEnable) return;
+    if (!CMSampleBufferIsValid(sampleBuffer)) return;
+    
+    CVPixelBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    if (!pixelBuffer) return;
+    
+    CVPixelBufferLockBaseAddress(pixelBuffer, 0);
+    
+    void *baseAddr = CVPixelBufferGetBaseAddress(pixelBuffer);
+    size_t width  = CVPixelBufferGetWidth(pixelBuffer);
+    size_t height = CVPixelBufferGetHeight(pixelBuffer);
+    size_t stride = CVPixelBufferGetBytesPerRow(pixelBuffer);
+    
+    // BGRA 蓝色: 0xFFFF0000
+    uint32_t *ptr = (uint32_t *)baseAddr;
+    for(size_t y = 0; y < height; y++){
+        uint32_t *row = ptr + (y * stride / 4);
+        for(size_t x = 0; x < width; x++){
+            row[x] = 0xFFFF0000;
+        }
+    }
+    
+    CVPixelBufferUnlockBaseAddress(pixelBuffer,0);
+}
+
+@end
+
+// 悬浮按钮UI
+@interface VCamFloatWindow : UIWindow
+@end
+@implementation VCamFloatWindow
+- (instancetype)init
+{
     self = [super init];
     if(self){
-        self.frame = CGRectMake(300,300,60,60);
+        self.frame = CGRectMake(50,200,70,70);
         self.windowLevel = UIWindowLevelAlert + 100;
-        self.backgroundColor = [UIColor blueColor];
-        self.layer.cornerRadius = 30;
+        self.backgroundColor = [UIColor systemBlueColor];
+        self.layer.cornerRadius = 35;
+        self.clipsToBounds = YES;
         self.hidden = NO;
+        
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
         btn.frame = self.bounds;
         [btn setTitle:@"VC" forState:UIControlStateNormal];
-        [btn addTarget:self action:@selector(onClick) forControlEvents:UIControlEventTouchUpInside];
+        [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        btn.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+        [btn addTarget:self action:@selector(tapBtn) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:btn];
     }
     return self;
 }
-- (void)onClick{
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"虚拟相机" message:@"开启后画面填充蓝色" preferredStyle:UIAlertControllerStyleAlert];
-    [alert addAction:[UIAlertAction actionWithTitle:@"开启" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-        g_virtualCamEnable = YES;
-    }]];
-    [alert addAction:[UIAlertAction actionWithTitle:@"关闭" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
-        g_virtualCamEnable = NO;
-    }]];
-    [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil]];
-    UIViewController *topVC = [self topViewController];
-    [topVC presentViewController:alert animated:YES completion:nil];
-}
-- (UIViewController *)topViewController {
-    UIViewController *vc = [UIApplication sharedApplication].keyWindow.rootViewController;
-    while (vc.presentedViewController) vc = vc.presentedViewController;
-    return vc;
+
+- (void)tapBtn
+{
+    g_vcamEnable = !g_vcamEnable;
+    NSLog(@"VirtualCam: %@", g_vcamEnable ? @"ON" : @"OFF");
+    self.backgroundColor = g_vcamEnable ? [UIColor systemRedColor] : [UIColor systemBlueColor];
 }
 @end
 
-static VCWindow *vcWin;
+static VCamFloatWindow *g_floatWin = nil;
 
 __attribute__((constructor))
-void lib_main() {
-    // dlsym 动态查找符号，不需要任何hook库
-    orig_CVPixelBufferLockBaseAddress = dlsym(RTLD_DEFAULT, "CVPixelBufferLockBaseAddress");
-    if (orig_CVPixelBufferLockBaseAddress) {
-        // 函数指针交换
-        *(void **)&orig_CVPixelBufferLockBaseAddress = hook_CVPixelBufferLockBaseAddress;
-    }
-
+void lib_main()
+{
+    // swizzle 替换方法
+    Class cls = objc_getClass("AVCaptureOutput");
+    SEL origSel = @selector(captureOutput:didOutputSampleBuffer:fromConnection:);
+    SEL newSel  = @selector(vcam_captureOutput:didOutputSampleBuffer:fromConnection:);
+    swizzle(cls, origSel, newSel);
+    
+    // 延迟创建悬浮窗，等主线程runloop
     dispatch_async(dispatch_get_main_queue(), ^{
-        vcWin = [[VCWindow alloc] init];
+        g_floatWin = [[VCamFloatWindow alloc] init];
     });
 }
+
+@end
