fbb8bb3060a4332ef79f9eb4d27ca0bd58178346
Allen9527466 <fff1191938336@gmail.com>
2026-09-11 18:28:13 +0800

    Update main.m

--- main.m
+++ main.m
@@ -3,10 +3,34 @@
 #import <Metal/Metal.h>
 #import <QuartzCore/QuartzCore.h>
 #include <objc/runtime.h>
+#include <stdatomic.h>
 
-static BOOL g_vcamEnable = NO;
+static atomic_bool g_vcamEnable = ATOMIC_VAR_INIT(0);
 static id<MTLTexture> g_imageTex = nil;
 static id<MTLDevice> g_mtlDev = nil;
+static id<MTLCommandQueue> g_cmdQueue = nil;
+static id<MTLRenderPipelineState> g_pipeline = nil;
+
+static const char* shaderSource =
+"#include <metal_stdlib>\n"
+"using namespace metal;\n"
+"struct VertexIn {\n"
+"    float2 pos [[attribute(0)]];\n"
+"    float2 uv [[attribute(1)]];\n"
+"};\n"
+"struct VertexOut {\n"
+"    float4 pos [[position]];\n"
+"    float2 uv;\n"
+"};\n"
+"vertex VertexOut vtx(VertexIn in [[stage_in]]) {\n"
+"    VertexOut out;\n"
+"    out.pos = float4(in.pos, 0, 1);\n"
+"    out.uv = in.uv;\n"
+"    return out;\n"
+"}\n"
+"fragment float4 frag(VertexOut in [[stage_in]], texture2d<float> tex [[texture(0)]]) {\n"
+"    return tex.sample(sampler(coord::clamp_to_edge, filter::linear), in.uv);\n"
+"}";
 
 static void swizzle(Class cls, SEL origSel, SEL newSel)
 {
@@ -15,7 +39,26 @@ static void swizzle(Class cls, SEL origSel, SEL newSel)
     method_exchangeImplementations(origMethod, newMethod);
 }
 
-// UIImage转Metal Texture
+static void buildPipeline()
+{
+    NSError *err = nil;
+    id<MTLLibrary> lib = [g_mtlDev newLibraryWithSource:@(shaderSource) options:nil error:&err];
+    if(err) {
+        NSLog(@"shader compile error: %@", err);
+        return;
+    }
+    id<MTLFunction> vtxFunc = [lib newFunctionWithName:@"vtx"];
+    id<MTLFunction> fragFunc = [lib newFunctionWithName:@"frag"];
+    
+    MTLRenderPipelineDescriptor *pipeDesc = [MTLRenderPipelineDescriptor new];
+    pipeDesc.vertexFunction = vtxFunc;
+    pipeDesc.fragmentFunction = fragFunc;
+    pipeDesc.colorAttachments[0].pixelFormat = MTLPixelFormatBGRA8Unorm;
+    
+    g_pipeline = [g_mtlDev newRenderPipelineStateWithDescriptor:pipeDesc error:&err];
+    if(err) NSLog(@"pipeline error: %@", err);
+}
+
 static id<MTLTexture> createTextureFromImage(UIImage *img, id<MTLDevice> dev)
 {
     if(!img || !dev) return nil;
@@ -24,9 +67,15 @@ static id<MTLTexture> createTextureFromImage(UIImage *img, id<MTLDevice> dev)
     
     size_t w = CGImageGetWidth(cgImg);
     size_t h = CGImageGetHeight(cgImg);
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGBA();
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
     void *bitmapData = malloc(w * h * 4);
-    CGContextRef ctx = CGContextCreate(bitmapData, w, h, 8, w*4, colorSpace, kCGImageAlphaPremultipliedLast);
+    if(!bitmapData)
+    {
+        CGColorSpaceRelease(colorSpace);
+        return nil;
+    }
+    CGBitmapInfo bitmapInfo = kCGImageAlphaPremultipliedLast;
+    CGContextRef ctx = CGBitmapContextCreate(bitmapData, w, h, 8, w*4, colorSpace, bitmapInfo);
     CGColorSpaceRelease(colorSpace);
     if(!ctx)
     {
@@ -44,42 +93,53 @@ static id<MTLTexture> createTextureFromImage(UIImage *img, id<MTLDevice> dev)
     return tex;
 }
 
-// 加载图片（路径：Documents/vcam_bg.png）
 static void loadBackgroundImage()
 {
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *docPath = paths.firstObject;
-    NSString *imgPath = [docPath stringByAppendingPathComponent:@"vcam_bg.png"];
-    UIImage *img = [UIImage imageWithContentsOfFile:imgPath];
-    if(!img)
-    {
-        NSLog(@"vcam_bg.png not found at %@", imgPath);
-        return;
+    @autoreleasepool {
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *docPath = paths.firstObject;
+        NSString *imgPath = [docPath stringByAppendingPathComponent:@"vcam_bg.png"];
+        UIImage *img = [UIImage imageWithContentsOfFile:imgPath];
+        if(!img)
+        {
+            NSLog(@"vcam_bg.png not found at %@", imgPath);
+            return;
+        }
+        if(!g_mtlDev) {
+            g_mtlDev = MTLCreateSystemDefaultDevice();
+            if(!g_mtlDev) {
+                NSLog(@"Metal device not supported");
+                return;
+            }
+            g_cmdQueue = [g_mtlDev newCommandQueue];
+            buildPipeline();
+        }
+        if(g_imageTex) g_imageTex = nil;
+        g_imageTex = createTextureFromImage(img, g_mtlDev);
+        NSLog(@"load image texture ok");
     }
-    if(!g_mtlDev) g_mtlDev = MTLCreateSystemDefaultDevice();
-    g_imageTex = createTextureFromImage(img, g_mtlDev);
-    NSLog(@"load image texture ok");
 }
 
-// 判断当前页面是否是抖音拍摄页
 static BOOL isCameraRecordingPage()
 {
-    UIWindow *keyWin = nil;
-    for(UIWindow *w in [UIApplication sharedApplication].windows)
-    {
-        if(w.isKeyWindow)
+    @autoreleasepool {
+        UIWindow *keyWin = nil;
+        for(UIWindow *w in [UIApplication sharedApplication].windows)
         {
-            keyWin = w;
-            break;
+            if(w.isKeyWindow)
+            {
+                keyWin = w;
+                break;
+            }
         }
+        if(!keyWin) return NO;
+        NSString *pageStr = [keyWin.rootViewController description];
+        if([pageStr containsString:@"Recorder"] || [pageStr containsString:@"Capture"])
+        {
+            return YES;
+        }
+        return NO;
     }
-    if(!keyWin) return NO;
-    NSString *pageStr = [keyWin.rootViewController description];
-    if([pageStr containsString:@"Recorder"] || [pageStr containsString:@"Capture"])
-    {
-        return YES;
-    }
-    return NO;
 }
 
 @interface CAMetalLayer (VCamHook)
@@ -89,37 +149,38 @@ static BOOL isCameraRecordingPage()
 @implementation CAMetalLayer (VCamHook)
 - (id<CAMetalDrawable>)vcam_nextDrawable
 {
-    id<CAMetalDrawable> drawable = [self vcam_nextDrawable];
-    if(!g_vcamEnable || !drawable) return drawable;
-    if(!isCameraRecordingPage()) return drawable;
-    
-    CGRect layerBounds = self.bounds;
-    CGFloat w = layerBounds.size.width;
-    CGFloat h = layerBounds.size.height;
-    if(w < 400 || h < 400) return drawable;
-    
-    if(!g_imageTex)
-    {
-        loadBackgroundImage();
+    @autoreleasepool {
+        id<CAMetalDrawable> drawable = [self vcam_nextDrawable];
+        if(!atomic_load(&g_vcamEnable) || !drawable) return drawable;
+        if(!isCameraRecordingPage()) return drawable;
+        if(!g_imageTex || !g_cmdQueue || !g_pipeline) return drawable;
+        
+        MTLRenderPassDescriptor *rpDesc = [MTLRenderPassDescriptor renderPassDescriptor];
+        rpDesc.colorAttachments[0].texture = drawable.texture;
+        rpDesc.colorAttachments[0].loadAction = MTLLoadActionClear;
+        rpDesc.colorAttachments[0].clearColor = MTLClearColorMake(0,0,0,1);
+        rpDesc.colorAttachments[0].storeAction = MTLStoreActionStore;
+        
+        id<MTLCommandBuffer> cmdBuf = [g_cmdQueue commandBuffer];
+        id<MTLRenderCommandEncoder> enc = [cmdBuf renderCommandEncoderWithDescriptor:rpDesc];
+        [enc setRenderPipelineState:g_pipeline];
+        
+        // 全屏三角带，覆盖整个屏幕
+        float quad[] = {
+            -1,-1, 0,1,
+             1,-1, 1,1,
+            -1, 1, 0,0,
+             1, 1, 1,0,
+        };
+        [enc setVertexBytes:quad length:sizeof(quad) atIndex:0];
+        [enc setFragmentTexture:g_imageTex atIndex:0];
+        [enc drawPrimitives:MTLPrimitiveTypeTriangleStrip vertexStart:0 vertexCount:4];
+        [enc endEncoding];
+        
+        [cmdBuf commit];
+        [cmdBuf waitUntilCompleted];
+        return drawable;
     }
-    if(!g_imageTex) return drawable;
-    
-    id<MTLTexture> dstTex = drawable.texture;
-    id<MTLCommandQueue> queue = [g_mtlDev newCommandQueue];
-    id<MTLCommandBuffer> cmd = [queue commandBuffer];
-    
-    MTLRenderPassDescriptor *rpd = [MTLRenderPassDescriptor renderPassDescriptor];
-    rpd.colorAttachments[0].texture = dstTex;
-    rpd.colorAttachments[0].loadAction = MTLLoadActionClear;
-    rpd.colorAttachments[0].clearColor = MTLClearColorMake(0,0,0,1);
-    
-    id<MTLRenderCommandEncoder> enc = [cmd renderCommandEncoderWithDescriptor:rpd];
-    // 简单贴图：直接拷贝纹理到画布（拉伸铺满预览画面）
-    [enc copyTextureFrom:g_imageTex sourceSlice:0 sourceLevel:0 sourceOrigin:MTLOriginMake(0,0,0) sourceSize:MTLSizeMake(g_imageTex.width, g_imageTex.height,1) toTexture:dstTex destinationSlice:0 destinationLevel:0 destinationOrigin:MTLOriginMake(0,0,0)];
-    [enc endEncoding];
-    [cmd commit];
-    
-    return drawable;
 }
 @end
 
@@ -151,11 +212,11 @@ static BOOL isCameraRecordingPage()
 
 - (void)tapBtn
 {
-    g_vcamEnable = !g_vcamEnable;
-    NSLog(@"VirtualCam toggle: %@", g_vcamEnable ? @"ON" : @"OFF");
-    self.backgroundColor = g_vcamEnable ? [UIColor systemRedColor] : [UIColor systemBlueColor];
-    // 每次切换开关，重新加载图片
-    if(g_vcamEnable) loadBackgroundImage();
+    atomic_bool old = atomic_load(&g_vcamEnable);
+    atomic_store(&g_vcamEnable, !old);
+    NSLog(@"VirtualCam toggle: %@", atomic_load(&g_vcamEnable) ? @"ON" : @"OFF");
+    self.backgroundColor = atomic_load(&g_vcamEnable) ? [UIColor systemRedColor] : [UIColor systemBlueColor];
+    if(atomic_load(&g_vcamEnable)) loadBackgroundImage();
 }
 @end
 
